0fb3de2369557a4cb405c97be415c11d3081a458
ChrisChan888 <oscaryiu.lapsang@gmail.com>
2022-06-09 22:09:32 +0800

    preload image

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,4 +1,9 @@
 import * as starImgSrc from './images/icon-star.svg'
 import * as paymentImgSrc from './images/illustration-thank-you.svg'
 
+// preload image
+;[starImgSrc, paymentImgSrc].map((src) => {
+  const img = new Image()
+  img.src = src.default
+})
 export { starImgSrc, paymentImgSrc }
